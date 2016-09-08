1e8781b6e467a9f1000dd82b7faa3cff03f3f0d9
ML <ml@gmail.com>
2016-09-08 09:56:23 -0400

    * Initial project setup

---
+++ Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seeker
+{
+    class Database
+    {
+        const string CONN_STRING = @"Data Source=ipd8.database.windows.net;Initial Catalog=seeker_v1;Integrated Security=False;User ID=ipd8abbott;Password=Abbott2000;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private SqlConnection conn;
+        // open the connection to the database
+        public Database()
+        {
+            conn = new SqlConnection(CONN_STRING);
+            conn.Open();
+        }
+    }
+}
